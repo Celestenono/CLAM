4627e8f2f74a480e9295b02d8b77482fca7a57ce
Celestenono <noemie.moreau96@gmail.com>
2024-10-30 14:56:01 +0100

    create splits

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -20,25 +20,24 @@ parser.add_argument('--test_frac', type=float, default= 0.1,
 
 args = parser.parse_args()
 
-if args.task == 'task_1_tumor_vs_normal':
+if args.task == 'task_1_tma':
     args.n_classes=2
-    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/tumor_vs_normal_dummy_clean.csv',
+    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/long_vs_short_survival_tma_train.csv',
                             shuffle = False, 
                             seed = args.seed, 
                             print_info = True,
-                            label_dict = {'normal_tissue':0, 'tumor_tissue':1},
+                            label_dict = {'long_survival': 0, 'short_survival': 1},
                             patient_strat=True,
                             ignore=[])
 
-elif args.task == 'task_2_tumor_subtyping':
-    args.n_classes=3
-    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/tumor_subtyping_dummy_clean.csv',
+elif args.task == 'task_2_wsi':
+    args.n_classes=2
+    dataset = Generic_WSI_Classification_Dataset(csv_path = 'dataset_csv/long_vs_short_survival_wsi_train.csv',
                             shuffle = False, 
                             seed = args.seed, 
                             print_info = True,
-                            label_dict = {'subtype_1':0, 'subtype_2':1, 'subtype_3':2},
+                            label_dict = {'long_survival': 0, 'short_survival': 1},
                             patient_strat= True,
-                            patient_voting='maj',
                             ignore=[])
 
 else:
