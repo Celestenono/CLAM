fa425cbd9ca0911dddd694f9a1a743d1a06614b5
Celestenono <noemie.moreau96@gmail.com>
2024-10-31 11:24:50 +0100

    no test set

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -61,8 +61,8 @@ if __name__ == '__main__':
             dataset.set_splits()
             descriptor_df = dataset.test_split_gen(return_descriptor=True)
             splits = dataset.return_splits(from_id=True)
-            save_splits(splits, ['train', 'val', 'test'], os.path.join(split_dir, 'splits_{}.csv'.format(i)))
-            save_splits(splits, ['train', 'val', 'test'], os.path.join(split_dir, 'splits_{}_bool.csv'.format(i)), boolean_style=True)
+            save_splits(splits, ['train', 'val'], os.path.join(split_dir, 'splits_{}.csv'.format(i)))
+            save_splits(splits, ['train', 'val'], os.path.join(split_dir, 'splits_{}_bool.csv'.format(i)), boolean_style=True)
             descriptor_df.to_csv(os.path.join(split_dir, 'splits_{}_descriptor.csv'.format(i)))
 
 
